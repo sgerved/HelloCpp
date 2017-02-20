9b3ecb1e4421928a127371f77f2da811d5785046
= <=>
2017-02-20 19:09:50 +0100

    Changes for added new files and replacing main

--- main.cpp
+++ main.cpp
@@ -1,20 +1,35 @@
 #include <iostream>
-#include <string>
+#include "functions.h"
 
 using namespace std;
 
-int main(int argc, char *argv[])
+/**
+ * Comment out the argc and argv, so we don't get
+ * any compiler warnings.
+ * @brief main
+ * @return
+ */
+int main(int /*argc*/, char /**argv[]*/)
 {
-//    cout << "Hello World!" << endl;
-//    int age;
-//    cout << "Enter age: ";
-//    cin >> age;
-//    cout << endl << "Hello, " << age;
-
-    cout << "Tell me your name: ";
-    string name;
-    cin >> name;
-    cout << "Hello, " << name;
-
+    if (add(1, 2) == 3) {
+        return 0;
+    }
+    cout << "Welcome to Guess My Number" << endl;
+    int answer = 7;
+    bool keepgoing = true;
+    while (keepgoing) {
+        cout << "Guess: ";
+        int guess;
+        cin >> guess;
+        if (guess < answer) {
+            cout << "Guess is smaller then answer";
+        } else if (guess > answer) {
+            cout << "Guess is larger then answer";
+        } else if (guess == answer) {
+            cout << "Correct guess!";
+            keepgoing = false;
+        }
+        cout << endl;
+    }
     return 0;
 }
