d080a8554ba63fda39f97b1dba7c9737c5210fef
Steffen Gerved <s.gerved@gerved.eu>
2017-02-20 18:51:57 +0100

    Init commit with project

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+//    cout << "Hello World!" << endl;
+//    int age;
+//    cout << "Enter age: ";
+//    cin >> age;
+//    cout << endl << "Hello, " << age;
+
+    cout << "Tell me your name: ";
+    string name;
+    cin >> name;
+    cout << "Hello, " << name;
+
+    return 0;
+}
